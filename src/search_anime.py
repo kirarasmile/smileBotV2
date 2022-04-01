2b9a7a5b410d28ed9064f4150f6311ff4ffb5025
kirarasmile <2839224397@qq.com>
2022-04-01 12:31:03 +0800

    修复搜图

--- src/search_anime.py
+++ src/search_anime.py
@@ -11,10 +11,10 @@ async def searchanime(bot, event):  # 搜索番剧截图
 # anime
     data_anime = requests.get("https://api.trace.moe/search?url={}".format(urllib.parse.quote_plus(animeimg))).json()
     try:
-        if data_anime['result'][0]['similarity'] > 0.90:
+        if data_anime['result'][0]['similarity'] > 90:
             # anicount += 1
             best_anime = data_anime['result'][0]
-            pic_anime = '[CQ:image,file=' + img + ']'
+            pic_anime = '[CQ:image,file=' + animeimg + ']'
             time_anime = str(best_anime['from']/60)
             title_anime = best_anime['filename']
             similarity_anime = str(best_anime['similarity'])
@@ -32,13 +32,13 @@ async def searchanime(bot, event):  # 搜索番剧截图
     sauce = SauceNao(api_key = config.saucenao_token,db=999)
     data_saucenao = sauce.from_url(animeimg)
     try:
-        if data_saucenao[0].similarity > 0.90:
+        if data_saucenao[0].similarity > 90:
             best_saucenao = data_saucenao[0]
-            pic_saucenao = '[CQ:image,file=' + url + ']'
+            pic_saucenao = '[CQ:image,file=' + animeimg + ']'
             title_saucenao = best_saucenao.title
             author_saucenao = best_saucenao.author
-            similarity_saucenao = best_saucenao.similarity
-            picurl_saucenao = best_saucenao.urls
+            similarity_saucenao = str(best_saucenao.similarity)
+            picurl_saucenao = best_saucenao.urls[0]
             await bot.send(event, message = pic_saucenao + "\n"+ \
                                                     "图片名：" + title_saucenao +  "\n" + \
                                                     "作者：" + author_saucenao + "\n" + \
